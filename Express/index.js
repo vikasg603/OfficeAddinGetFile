cda33bc1ab9cb72d708954b188e6f2bc2008ad78
Vikas <vikas.rg@somaiya.edu>
2021-07-02 14:14:55 +0530

    express

---
+++ Express/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs/promises');
+const uuid = require('uuid');
+const { Calibre } = require('node-calibre');
+
+const TempFilePath = './temp/';
+
+app.use(express.json());
+app.use(express.static(TempFilePath));
+
+app.post('/ProcessBase64PDF', async (req, res) => {
+    try {
+
+        if(!req.body.doc) {
+            res.status(400).json({
+                Error: "No doc found"
+            });
+        }
+        const base64Doc = req.body.doc;
+
+        const FilePath = TempFilePath + uuid.v4() + ".pdf"
+
+        await fs.writeFile(FilePath, base64Doc, 'base64');
+
+        const calibre = new Calibre();
+
+        const newFile = await calibre.ebookConvert(FilePath, 'epub', {
+            epubFlatten: null,
+        });
+
+        res.send({path: newFile});
+
+    } catch(err) {
+        res.status(503).json({
+            Error: "Internal server error"
+        });
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
