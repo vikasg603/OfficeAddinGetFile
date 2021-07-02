d7a3f343522a580801dc0cf86a214c67ba71f26a
Vikas <vikas.rg@somaiya.edu>
2021-07-02 15:35:59 +0530

    cors

--- Express/index.js
+++ Express/index.js
@@ -11,6 +11,8 @@ const TempFilePath = './temp/';
 app.use(express.json());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://vikasg603.github.io');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 //app.use(cors());
