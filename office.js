67b2a1f429577a9591d1ddd245c0f09e63a6b0ec
Vikas <vikas.rg@somaiya.edu>
2021-07-02 13:01:52 +0530

    btoa

--- office.js
+++ office.js
@@ -41,7 +41,7 @@ function onGotAllSlices(docdataSlices) {
 		fileContent += String.fromCharCode(docdata[j]);
 	}
 
-	console.log(fileContent);
+	console.log(window.btoa(fileContent));
 
 	// Now all the file content is stored in 'fileContent' variable,
 	// you can do something with it, such as print, fax...
